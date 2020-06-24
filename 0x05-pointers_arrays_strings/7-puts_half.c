c0bcc6a6c3d77eef81855d20c2651a9a0d77a366
ClaraCastaD <1980@holbertonschool.com>
2020-06-24 23:11:10 +0000

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts_half - function that prints half of a string
+ *@str: Other character of the string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+		;
+	j = j + 1;
+	for (j = j / 2; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
