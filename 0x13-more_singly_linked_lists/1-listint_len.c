6c66789ae82ffd9322e0dbd614531bfbc1842fd3
ClaraCastaD <1980@holbertonschool.com>
2020-07-29 15:34:53 +0000

    Uploading task number one

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: elemento of the linked list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
