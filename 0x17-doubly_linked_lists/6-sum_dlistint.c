2da297998cedbeb68f6d7b825881dfbf43adb61c
ClaraCastaD <1980@holbertonschool.com>
2020-09-13 20:31:15 +0000

    Adding task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - summ the numbers of a dlistint_t list.
+ * @head: pointer to the first element of the list
+ * Return: no return
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum;
+
+	sum = 0;
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum = head->n + sum;
+		head = head->next;
+	}
+	return (sum);
+}
